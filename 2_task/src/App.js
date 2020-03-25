00f0a90ad6dc35a79f4acc428ec62ebae6adb3fc
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-03-25 14:27:58 +0200

    changed to class component

--- 2_task/src/App.js
+++ 2_task/src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-      <Person name="Phuong" age="24" />
-      <Person name="Kati" age="25" />
-      <Person name="Karin" age="45" />
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Phuong', age: 24 },
+      { name: 'Kati', age: 25 },
+      { name: 'Karin', age: 45 }
+    ]
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    )
+  }
+}
+
 
 export default App;
