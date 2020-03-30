d49d49da41806d7327071b3ec772fed3130c22e9
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-03-30 11:10:10 +0300

    added like, diskie and reset buttons and add setState

--- 3_task/src/App.js
+++ 3_task/src/App.js
@@ -5,10 +5,31 @@ class App extends Component {
   state = {
     likes: 0
   };
+
+  addLike = () => {
+    this.setState({
+      likes: this.state.likes + 1
+    });
+  };
+
+  removeLike = () => {
+    this.setState({
+      likes: this.state.likes - 1
+    });
+  };
+
+  reset = () => {
+    this.setState({
+      likes: 0
+    });
+  };
   render() {
     return (
       <div>
         <h1>Total Likes: {this.state.likes}</h1>
+        <button onClick={this.addLike}>Like</button>
+        <button onClick={this.removeLike}>Dislike</button>
+        <button onClick={this.reset}>Reset</button>
       </div>
     );
   }
