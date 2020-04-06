8d76904aad2f4da81d610ae08c837e94c21d45a8
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-04-06 13:50:33 +0300

    find random number for random circle highlight

--- 5_task/src/App.js
+++ 5_task/src/App.js
@@ -2,20 +2,43 @@ import "./App.css";
 import React, { Component } from "react";
 import Circle from "./Circle/Circle";
 
+const getRndInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
 class App extends Component {
   state = {
     score: 0,
+    current: 0,
+  };
+
+  next = () => {
+    let nextActive = undefined;
+    do {
+      nextActive = getRndInteger(1, 4);
+    } while (nextActive === this.state.current);
+
+    this.setState({
+      current: nextActive,
+    });
+  };
+
+  clickHandler = (circleID) => {
+    console.log("clicked", circleID);
+    this.setState({
+      score: this.state.score + 1,
+    });
   };
   render() {
     return (
       <div>
         <h1>Smack The Virus</h1>
-        <h3>Your score is: 0</h3>
+        <h3>Your score is: {this.state.score}</h3>
         <main>
-          <Circle />
-          <Circle />
-          <Circle />
-          <Circle />
+          <Circle clicks={this.clickHandler.bind(this, 1)} />
+          <Circle clicks={this.clickHandler.bind(this, 2)} />
+          <Circle clicks={this.clickHandler.bind(this, 3)} />
+          <Circle clicks={this.clickHandler.bind(this, 4)} />
         </main>
         <div>
           <button>Start</button>
--- 5_task/src/Circle/Circle.js
+++ 5_task/src/Circle/Circle.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Circle.css";
 
-const Circle = () => {
-  return <div className="circle"></div>;
+const Circle = (props) => {
+  return <div className="circle" onClick={props.clicks}></div>;
 };
 
 export default Circle;
