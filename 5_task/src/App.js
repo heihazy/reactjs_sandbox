49919150f86a2eb670d07fa39b7e230d2042912c
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-04-06 14:52:16 +0300

    add timer for random numbers and highlight circle corlor

--- 5_task/src/App.js
+++ 5_task/src/App.js
@@ -12,6 +12,9 @@ class App extends Component {
     current: 0,
   };
 
+  pace = 1500;
+  timer = undefined;
+
   next = () => {
     let nextActive = undefined;
     do {
@@ -21,6 +24,8 @@ class App extends Component {
     this.setState({
       current: nextActive,
     });
+    this.timer = setTimeout(this.next, this.pace);
+    console.log(this.state.current);
   };
 
   clickHandler = (circleID) => {
@@ -29,20 +34,44 @@ class App extends Component {
       score: this.state.score + 1,
     });
   };
+
+  startHandler = () => {
+    this.next();
+  };
+
+  endHandler = () => {
+    clearTimeout(this.timer);
+  };
   render() {
     return (
       <div>
         <h1>Smack The Virus</h1>
         <h3>Your score is: {this.state.score}</h3>
         <main>
-          <Circle clicks={this.clickHandler.bind(this, 1)} />
-          <Circle clicks={this.clickHandler.bind(this, 2)} />
-          <Circle clicks={this.clickHandler.bind(this, 3)} />
-          <Circle clicks={this.clickHandler.bind(this, 4)} />
+          <Circle
+            active={this.state.current === 1}
+            buttonColor="lightblue"
+            clicks={this.clickHandler.bind(this, 1)}
+          />
+          <Circle
+            active={this.state.current === 2}
+            buttonColor="lightsalmon"
+            clicks={this.clickHandler.bind(this, 2)}
+          />
+          <Circle
+            active={this.state.current === 3}
+            buttonColor="lightgreen"
+            clicks={this.clickHandler.bind(this, 3)}
+          />
+          <Circle
+            active={this.state.current === 4}
+            buttonColor="lightcyan"
+            clicks={this.clickHandler.bind(this, 4)}
+          />
         </main>
         <div>
-          <button>Start</button>
-          <button>End</button>
+          <button onClick={this.startHandler}>Start</button>
+          <button onClick={this.endHandler}>End</button>
         </div>
       </div>
     );
--- 5_task/src/Circle/Circle.js
+++ 5_task/src/Circle/Circle.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./Circle.css";
 
 const Circle = (props) => {
-  return <div className="circle" onClick={props.clicks}></div>;
+  return (
+    <div
+      className={"circle" + (props.active ? " active" : " ")}
+      onClick={props.clicks}
+      style={{
+        backgroundColor: props.active ? props.active : props.buttonColor,
+      }}
+    ></div>
+  );
 };
 
 export default Circle;
