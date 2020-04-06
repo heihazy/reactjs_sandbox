d9a4287fd603ea24077f65e91b72071f94b69fa6
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-04-06 12:52:34 +0300

    created and styled circle component

--- 5_task/src/App.js
+++ 5_task/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import React, { Component } from "react";
+import Circle from "./Circle/Circle";
 
 class App extends Component {
   state = {
@@ -8,10 +9,18 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Smash The Virus</h1>
+        <h1>Smack The Virus</h1>
         <h3>Your score is: 0</h3>
-        <button>Start</button>
-        <button>End</button>
+        <main>
+          <Circle />
+          <Circle />
+          <Circle />
+          <Circle />
+        </main>
+        <div>
+          <button>Start</button>
+          <button>End</button>
+        </div>
       </div>
     );
   }
