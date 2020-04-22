2b1e173adaf9a81c92925be624028df89032149a
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-04-22 12:50:24 +0300

    import useState and add states for buttons

--- 7_task/src/App.js
+++ 7_task/src/App.js
@@ -1,9 +1,20 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class App extends Component {
-  render() {
-    return <div></div>;
-  }
-}
+const App = () => {
+  const [vote, setVote] = useState(0);
+  const addLove = () => setVote(vote + 1);
+  const addHate = () => setVote(vote + 1);
+  const resetVote = () => setVote(0);
+  return (
+    <div>
+      <button onClick={addLove}>Love Me</button>
+      <button onClick={addHate}>Hate Me</button>
+      <button onClick={resetVote}>Back to Neutral</button>
+      <h1>Votes:</h1>
+      <h3>Love: 0</h3>
+      <h3>Hate: 0</h3>
+    </div>
+  );
+};
 
 export default App;
